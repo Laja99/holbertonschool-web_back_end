c733a6df6e220af459824ea90679e58f2e512b74
laja99 <layla.jaber.m@gmail.com>
2026-05-09 00:04:01 +0300

    add name and class

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -18,6 +18,14 @@ export class StudentHolberton {
     this._holbertonClass = holbertonClass;
   }
 
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
