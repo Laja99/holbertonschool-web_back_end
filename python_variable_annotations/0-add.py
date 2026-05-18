fbcd8f2d0dad6e8c6356790c6408b72056396bfa
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-05-18 16:58:06 +0300

    add python_async_comprehension, python_async_function, python_variable_annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module provides a basic type-annotated function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b as a float."""
+
+    return a + b
